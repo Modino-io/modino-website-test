72b974026aa729aae01f6e98a60a358d9a3f1812
Kacper Kaźmierkiewicz <kacprzok@gmail.com>
2023-03-14 22:51:02 +0100

    feat: add first draft
    Modino-io/modino#554

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const menu = document.getElementsByClassName("m-menu")[0];
+const menuCtaButton = menu.getElementsByClassName("m-menu__cta-button")[0];
+
+const mainCtaButton = document.getElementById("main-cta-button");
+
+document.addEventListener("scroll", () => {
+  let scrollTop = window.scrollY;
+
+  if (scrollTop > 0) {
+    menu.classList.add("m-menu_sticky");
+  } else {
+    menu.classList.remove("m-menu_sticky");
+  }
+
+  if (mainCtaButton.getBoundingClientRect().bottom <= 50) {
+    menuCtaButton.classList.remove("mdc-button--outlined");
+    menuCtaButton.classList.add("mdc-button--raised");
+  } else {
+    menuCtaButton.classList.add("mdc-button--outlined");
+    menuCtaButton.classList.remove("mdc-button--raised");
+  }
+});
