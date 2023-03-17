12b97b0806c843550278a941181c9bb0a2f0048b
Kacper Kaźmierkiewicz <kacprzok@gmail.com>
2023-03-17 13:15:24 +0100

    feat: add ripple to the buttons
    Modino-io/modino#554

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ const menuCtaButton = menu.getElementsByClassName("m-menu__cta-button")[0];
 
 const mainCtaButton = document.getElementById("main-cta-button");
 
+const MDCRipple = mdc.ripple.MDCRipple;
+const mainCtaButtonRipple = new MDCRipple(
+  document.getElementById("main-cta-button")
+);
+const menuCtaButtonRipple = new MDCRipple(
+  menu.getElementsByClassName("m-menu__cta-button")[0]
+);
+
 document.addEventListener("scroll", () => {
   let scrollTop = window.scrollY;
 
@@ -14,9 +22,9 @@ document.addEventListener("scroll", () => {
 
   if (menuCtaButton && mainCtaButton.getBoundingClientRect().bottom <= 50) {
     menuCtaButton.classList.remove("mdc-button--outlined");
-    menuCtaButton.classList.add("mdc-button--raised");
+    menuCtaButton.classList.add("mdc-button--unelevated");
   } else {
     menuCtaButton.classList.add("mdc-button--outlined");
-    menuCtaButton.classList.remove("mdc-button--raised");
+    menuCtaButton.classList.remove("mdc-button--unelevated");
   }
 });
