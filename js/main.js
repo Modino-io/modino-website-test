5b2006463e795bae6bba354b3df09cbf9749ec8a
Jan Kwarciński <jankwarcin@gmail.com>
2023-07-13 15:01:04 +0200

    ci: add different copyright to sccc and js, css

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-/*
- * Modino.io Confidential
- *
- * Object code only source materials
- *
- * Modino.io - Safe and Secure updates - Platform for safe and secure software delivery
- *
- * (C) Copyright Modino.io Sp. z o.o. <copyright@modino.io> 2020-2023. All Rights Reserved.
- *
- * The source code for this program is not published or otherwise
- * divested of its trade secrets, irrespective of what has
- * been deposited with any of the Copyright Offices.
- */
-
 const menuCtaButton = document.getElementsByClassName("m-menu__cta-button")[0];
 let currentTab = "founders";
 const tabs = document.querySelectorAll(".m-section__item");
--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,3 @@
-/*
- * Modino.io Confidential
- *
- * Object code only source materials
- *
- * Modino.io - Safe and Secure updates - Platform for safe and secure software delivery
- *
- * (C) Copyright Modino.io Sp. z o.o. <copyright@modino.io> 2020-2023. All Rights Reserved.
- *
- * The source code for this program is not published or otherwise
- * divested of its trade secrets, irrespective of what has
- * been deposited with any of the Copyright Offices.
- */
-
 const menu = document.getElementsByClassName("m-menu")[0];
 const menuNav = document.getElementsByClassName("m-menu__navigation")[0];
 
