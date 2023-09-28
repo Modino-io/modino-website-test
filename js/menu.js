5028830e1ba9beb95a83d663720fed3030d59018
Kacper Kaźmierkiewicz <kacprzok@gmail.com>
2023-09-28 22:19:05 +0200

    feat(menu): add mobile menu
    Modino-io/modino#851

--- js/menu.js
+++ js/menu.js
@@ -6,8 +6,20 @@
 
 const menu = document.getElementsByClassName("m-menu")[0];
 const menuNav = document.getElementsByClassName("m-menu__navigation")[0];
+const hamburgerMenuButton = document.getElementsByClassName(
+  "m-menu__hamburger-button"
+)[0];
+/**
+ * @type {HTMLElement}
+ */
+const mobileMenu = document.getElementsByClassName("m-mobile-menu")[0];
 
 document.addEventListener("scroll", checkMenuCtaButton);
+
+if (hamburgerMenuButton) {
+  const hamburgerMenuRipple = new mdc.ripple.MDCRipple(hamburgerMenuButton);
+  window.addEventListener("resize", hamburgerMenuHandle);
+}
 
 checkMenuCtaButton();
 
@@ -19,7 +31,29 @@ function checkMenuCtaButton() {
   }
 }
 
+function toggleHamburgerMenu() {
+  mobileMenu.classList.toggle("m-mobile-menu--expanded");
+
+  if (mobileMenu.classList.contains("m-mobile-menu--expanded")) {
+    mobileMenu.style.display = "flex";
+    setTimeout(() => {
+      mobileMenu.style.transform = "none";
+    }, 100);
+  } else {
+    mobileMenu.style.transform = "translateX(100%)";
+    setTimeout(() => {
+      mobileMenu.style.display = "none";
+    }, 300);
+  }
+}
+
+/**
+ *
+ * @param {UIEvent} event
+ */
+function hamburgerMenuHandle(event) {
+  console.log(event.target.innerWidth);
+}
+
 const currentYear = new Date().getFullYear();
-console.log(document.querySelector("#footer-year"));
-console.log(currentYear);
 document.querySelector("#footer-year").innerHTML = currentYear;
