b25fcbeca1c98f54306cba75a3f2f07204196c39
Kacper Kaźmierkiewicz <kacprzok@gmail.com>
2024-03-05 15:48:29 +0100

    feat: create footer component
    Modino-io/modino#1090

---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import "./footer.component.scss";
+
+const Footer = () => {
+  return (
+    <footer className="m-footer">
+      <div className="m-footer__content">
+        <div className="m-footer__menu">
+          <div className="m-footer__menu-section m-footer__company-info">
+            <p className="m-footer__menu-section-title">Company</p>
+            <p>
+              Modino.io Sp. z.o.o <br />
+              EU VAT no.: PL7393955632 <br />
+              E-mail: hello@modino.io
+            </p>
+          </div>
+          <div className="m-footer__menu-section">
+            <p className="m-footer__menu-section-title">Explore</p>
+            <ul className="m-footer__menu-section-list">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/solution">Solution</Link>
+              </li>
+              <li>
+                <Link to="/articles">Articles</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="m-footer__menu-section">
+            <p className="m-footer__menu-section-title">Connect</p>
+            <ul className="m-footer__menu-section-list">
+              <li>
+                <Link to="/meet-the-team">Meet the team</Link>
+              </li>
+              <li>
+                <Link to="/partner-program">Partner Program</Link>
+              </li>
+              <li>
+                <Link to="/jobs">Jobs</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="m-footer__links">
+          <a
+            className="m-footer__social-link"
+            href="https://www.linkedin.com/company/modino-io"
+            target="_blank"
+            rel="noopener"
+          >
+            <img alt="linkedIn icon" src="/img/linkedin_white.svg" />
+          </a>
+          <div className="m-footer__copyright">
+            <p>
+              &#169; Modino.io <span id="footer-year"></span>. All rights
+              reserved.
+            </p>
+            <div className="m-footer__copyright-links">
+              <Link to="/privacy-policy">Privacy policy</Link>
+              <Link to="/terms-of-service">Terms of service</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Menu from "../menu/menu.component";
+import Footer from "../footer/footer.component";
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <React.Fragment>
       <Menu />
       {children}
+      <Footer />
     </React.Fragment>
   );
 };
