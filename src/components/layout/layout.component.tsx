9f4ed477b49b0099e75e55fde1e235cc38f24d3d
Kacper Kaźmierkiewicz <kacprzok@gmail.com>
2024-03-08 10:54:59 +0100

    feat: add styles for layout
    Modino-io/modino#1090

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import Menu from "../menu/menu.component";
 import Footer from "../footer/footer.component";
 
+import "./layout.component.scss";
+
 const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <React.Fragment>
