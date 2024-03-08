05b7777bcd62f6d1a869d590721ce263c5f18529
Kacper Kaźmierkiewicz <kacprzok@gmail.com>
2024-03-08 10:54:31 +0100

    feat: add jobs pages
    Modino-io/modino#1090

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -1,20 +1,42 @@
 import React from "react";
 import { HeadFC, Link, PageProps, graphql } from "gatsby";
-import SEO from "../../components/seo.component";
+import Seo from "../../components/seo.component";
 import Layout from "../../components/layout/layout.component";
+
+import "./index.scss";
 
 const JobsPage: React.FC<PageProps> = ({ data }) => {
   return (
     <Layout>
-      <main>Jobs page</main>
-      {data.allMdx.nodes.map((node) => (
-        <React.Fragment key={node.frontmatter.slug}>
-          <Link to={`/jobs/${node.frontmatter.slug}`}>
-            {node.frontmatter.title}
-          </Link>
-          <br />
-        </React.Fragment>
-      ))}
+      <main className="m-main">
+        <section className="m-section m-jobs">
+          <div className="m-section__content">
+            <div className="m-jobs__content">
+              <h1 className="m-section__header m-jobs__header">
+                Jobs at <span className="highlight">Modino.io</span>
+              </h1>
+              <div className="m-jobs__text">
+                Current openings:
+                <ul>
+                  {data.allMdx.nodes.map((node) => (
+                    <li key={node.frontmatter.slug}>
+                      <Link to={`/jobs/${node.frontmatter.slug}`}>
+                        {node.frontmatter.title}
+                      </Link>
+                      <br />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <img
+                className="m-jobs__image"
+                src="/img/jobs.svg"
+                alt="jobs hero image"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
     </Layout>
   );
 };
@@ -36,7 +58,7 @@ export const query = graphql`
 export default JobsPage;
 
 export const Head: HeadFC = () => (
-  <SEO
+  <Seo
     pageTitle="Modino.io - jobs opportunities"
     description="Check out our open job opportunities and help us shape the future of IoT security."
     link="https://modino.io/jobs"
--- src/pages/jobs/{mdx.frontmatter__slug}.tsx
+++ src/pages/jobs/{mdx.frontmatter__slug}.tsx
@@ -1,6 +1,81 @@
 import * as React from "react";
+import "./job.scss";
+import Layout from "../../components/layout/layout.component";
+import { HeadFC, Link, graphql } from "gatsby";
+import Seo from "../../components/seo.component";
 
-const BlogPost = ({ data, children }) => {
-  return <React.Fragment>{children}</React.Fragment>;
+const BlogPost = ({
+  data,
+  children,
+}: React.PropsWithChildren<{ data: object }>) => {
+  console.log(data);
+
+  return (
+    <Layout>
+      <main className="m-main">
+        <section className="m-section m-job">
+          <div className="m-section__content">
+            <div className="m-job__breadcrumbs">
+              <Link to="/" className="m-job__breadcrumb">
+                Home
+              </Link>{" "}
+              |
+              <Link to="../" className="m-job__breadcrumb">
+                Jobs
+              </Link>{" "}
+              |
+              <div className="m-job__breadcrumb">
+                {data.mdx.frontmatter.title}
+              </div>
+            </div>
+            <h1 className="m-section__header m-job__header">
+              {data.mdx.frontmatter.title}
+            </h1>
+            <div className="m-job__content">
+              <p>
+                <b>Location:</b> Bydgoszcz, Poland / Warsaw, Poland or remote –
+                Full time
+              </p>
+
+              {children}
+              <Link to="../" className="m-job__back-link">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24"
+                  viewBox="0 -960 960 960"
+                  width="24"
+                >
+                  <path d="m313-440 196 196q12 12 11.5 28T508-188q-12 11-28 11.5T452-188L188-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l264-264q11-11 27.5-11t28.5 11q12 12 12 28.5T508-715L313-520h447q17 0 28.5 11.5T800-480q0 17-11.5 28.5T760-440H313Z" />
+                </svg>
+                Go back to Jobs
+              </Link>
+            </div>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
 };
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        meta_description
+        slug
+        type
+      }
+    }
+  }
+`;
+
+export const Head: HeadFC = ({ data }) => (
+  <Seo
+    pageTitle={`Modino.io - ${data.mdx.frontmatter.title}`}
+    description={data.mdx.frontmatter.meta_description}
+    link={`https://modino.io/jobs/${data.mdx.frontmatter.slug}`}
+  />
+);
+
 export default BlogPost;
